c157de76662ce1968855cab551bc1a58ff35ec7a
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-28 15:04:56 +0000

    Made changes to this file

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,13 @@ import githubIcon from './images/github.svg';
 function Home() {
   return (
     <div className="home">
-      <h1>Ebenezer Antwi</h1>
+      <h1>Ebenezer </h1>
       <p>I'm a <span>Front-end Developer.</span></p>
       <NavBar/>
         <div className='social-icons'>
-          <a href="https://twitter.com/_antwiebenezer" target='_blank'><img src={twitterIcon} alt='twitter-icon'/></a>
-          <a href="https://www.linkedin.com/in/antwi-ebenezer/" target='_blank'><img src={linkedInIcon} alt='linkedIn-icon'/></a>
-          <a href="https://github.com/kwadwoambitious" target='_blank'><img src={githubIcon} alt='githun-icon'/></a>
+          <a href="https://twitter.com/_antwiebenezer" target='_blank'><img src={twitterIcon} alt='twitter-logo'/></a>
+          <a href="https://www.linkedin.com/in/antwi-ebenezer" target='_blank'><img src={linkedInIcon} alt='linkedIn-logo'/></a>
+          <a href="https://github.com/kwadwoambitious" target='_blank'><img src={githubIcon} alt='githun-logo'/></a>
         </div>
     </div>
   )

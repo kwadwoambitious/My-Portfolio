e24dcf0b9f23849365dd13c625b80fbca63e5949
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-28 08:32:07 +0000

    Made changes to this file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+         <Routes>
+            <Route exact path='/' element={<Home/>}>
+              
+            </Route>
+         </Routes>
+      </div>
+    </Router>
   );
 }
 

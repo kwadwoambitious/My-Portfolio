0f67dfba0bab067a1bbe50f34af5562f2417ee45
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-28 13:17:38 +0000

    Made changes to this file

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,9 @@ function Home() {
       <p>I'm a <span>Front-end Developer.</span></p>
       <NavBar/>
         <div className='social-icons'>
-          <img src={twitterIcon} alt='twitter-icon'/>
-          <img src={linkedInIcon} alt='linkedIn-icon'/>
-          <img src={githubIcon} alt='githun-icon'/>
+          <a href="https://twitter.com/_antwiebenezer" target='_blank'><img src={twitterIcon} alt='twitter-icon'/></a>
+          <a href="https://www.linkedin.com/in/antwi-ebenezer/" target='_blank'><img src={linkedInIcon} alt='linkedIn-icon'/></a>
+          <a href="https://github.com/kwadwoambitious" target='_blank'><img src={githubIcon} alt='githun-icon'/></a>
         </div>
     </div>
   )

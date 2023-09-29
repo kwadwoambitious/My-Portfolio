4d6895385ba65379a4027411d6f203355b71a383
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-29 13:06:14 +0000

    Made changes to this file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import twitterIcon from './images/twitter-x-fill.svg';
 import linkedInIcon from './images/linkedin-logo.svg';
 import githubIcon from './images/github.svg';
+import menuBar from './images/menu.svg';
 import NavBar from './NavBar';
 
 function Home() {
   return (
     <div className="home">
+      <img src={menuBar} alt="menu-bar" className='menu-bar'/>
       <h1>Ebenezer Antwi</h1>
       <p>I'm a <span>Front-end Developer.</span></p>
       <NavBar/>

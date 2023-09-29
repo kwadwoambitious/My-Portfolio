a76925d81fd9c8b8e5c4488e0e33ab2b1acdc8ac
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-29 12:38:59 +0000

    Made changes to this file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';

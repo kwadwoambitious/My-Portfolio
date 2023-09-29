f9d2d6bf15a0baa001880e62792eb8141f44bb1f
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-29 12:40:35 +0000

    Made changes to this file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import menuBar from './images/menu.svg';
+import closeBar from './images/close-thin.svg';
 import '../App.css';
 
 function NavBar() {
   return (
     <>
-       <nav>
-        <ul>
-          <li>
-            <Link to="/" className='nav-link active-link'>Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className='nav-link'>About</Link>
-          </li>
-          <li>
-            <Link to="/portfolio" className='nav-link'>Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/certificates" className='nav-link'>Certificates</Link>
-          </li>
-          <li>
-            <Link to="/contact" className='nav-link'>Contact</Link>
-          </li>
-        </ul>
-       </nav>
+      <div className='nav-container'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className='nav-link active-link'>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" className='nav-link'>About</Link>
+            </li>
+            <li>
+              <Link to="/portfolio" className='nav-link'>Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/certificates" className='nav-link'>Certificates</Link>
+            </li>
+            <li>
+              <Link to="/contact" className='nav-link'>Contact</Link>
+            </li>
+          </ul>
+          <img src={closeBar} alt="close-bar"/>
+        </nav>
+      </div>
     </>
   )
 }

4323c2dbdf0eaf2c7c377f788fc11d023bc7c900
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-29 12:39:40 +0000

    Made changes to this file

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <Header/>
+    </>
+    
   )
 }
 

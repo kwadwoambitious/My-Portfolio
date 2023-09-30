1276ba38cf8379c6a2234503b680621c8baefbad
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-30 14:52:17 +0000

    Made changes to this file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import menuBar from './images/menu.svg';
 import closeBar from './images/close-thin.svg';
 import '../App.css';
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  }
+
   return (
     <>
         <nav>
         <div className='nav-container'>
           <ul>
             <li>
-              <NavLink to="/" className='nav-link'>Home</NavLink>
+              <NavLink to="/" className='nav-linkf'>Home</NavLink>
             </li>
             <li>
               <NavLink to="/about" className='nav-link'>About</NavLink>
@@ -28,7 +34,7 @@ function NavBar() {
           </ul>
           <img src={closeBar} alt="close-bar" className='close-bar'/>
         </div>
-          <img src={menuBar} alt="menu-bar" className='menu-bar'/>
+          <img src={menuBar} alt="menu-bar" className='menu-bar' onClick={toggleMenu}/>
         </nav>
     </>
   )

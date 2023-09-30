5554bf7ffa13d75963b4b97f91c6ef53c8d00758
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-30 14:37:43 +0000

    Made changes to this file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header/>
          <Routes>
-            <Route exact path='/' element={<Home/>}>
-              
-            </Route>
-            <Route exact path='/about' element={<About/>}>
-              
-            </Route>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/about' element={<About/>}/>
          </Routes>
       </div>
     </Router>

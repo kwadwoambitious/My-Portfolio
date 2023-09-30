78f7dd0e8eee184050152432f64f0230b60f4ec3
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-30 14:37:54 +0000

    Made changes to this file

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import menuBar from './images/menu.svg';
+const AboutMe = () => {
+  return(
+    <>
+        <h2>About Me</h2>
+        <h3>Hello World</h3>
+    </>
+  )
+}
 
 const About = () => {
   return (
     <>
-      <Header/>
-      <img src={menuBar} alt="menu-bar" className='menu-bar'/>
+      <div className='about-container'>
+        <AboutMe/>
+      </div>
     </>
     
   )

79051e9f6a50012d7557cb1e16a2592430d5ab1a
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-03 16:55:21 +0000

    Made changes to this file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Certificates from './components/Certificates';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
          <Routes>
             <Route exact path='/' element={<Home/>}/>
             <Route exact path='/about' element={<About/>}/>
+            <Route exact path='/contact' element={<Contact/>}/>
+            <Route exact path='/certificates' element={<Certificates/>}/>
          </Routes>
       </div>
     </Router>

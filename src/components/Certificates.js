a2ae67c8bbe30e3139284db3d27c65386de976e9
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-03 16:55:53 +0000

    Made changes to this file

---
+++ src/components/Certificates.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Certificates = () => {
+  return (
+    <>
+      <div className="awards-container">
+        <h4>CERTIFICATES</h4>
+        <h2>MY CERTIFICATES</h2>
+      </div>
+    </>
+  )
+}
+
+export default Certificates;

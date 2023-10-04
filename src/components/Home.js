e04f307dd7ae3464111ad4f5d4e47ea6155e5fa6
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-04 10:39:56 +0000

    Made changes to this file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  useEffect(()=> {
+    Aos.init({duration: 2000});
+  }, [])
+
   return (
     <>
-      <div className="home">
+      <div className="home" data-aos="fade-in">
         <h1>Ebenezer Antwi</h1>
         <h2>I'm a <span>Front-end Developer.</span></h2>
           <div className='social-icons'>

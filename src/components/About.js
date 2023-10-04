ce665eaca28d8114ca803f6b6448db4b27c5022b
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-04 10:39:26 +0000

    Made changes to this file

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import htmlImg from '../images/skills/html.png';
 import cssImg from '../images/skills/css.png';
 import javascriptImg from '../images/skills/javascript.png';
@@ -117,9 +119,12 @@ const Services = () => {
 }
 
 const About = () => {
+  useEffect(()=> {
+    Aos.init({duration: 2000});
+  }, [])
   return (
     <>
-      <div className='about-container'>
+      <div className='about-container' data-aos="fade-in">
         <AboutMe/>
         <Skills/>
         <Interest/>

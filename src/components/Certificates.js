8d267fbbce0157a699661572a4869e8113e1a252
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-05 18:00:08 +0000

    Made changes to this file

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,36 +1,42 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React from 'react';
 import webDesign from '../components/images/certificates/web-design.png';
 import javaScript from '../components/images/certificates/Programming_with_JavaScript.png';
 import webDesign2 from '../components/images/certificates/Responsive-Web-Design_certificate.jpg';
 import htmlCourse from '../components/images/certificates/HTML_certificate.jpg';
+import { motion } from 'framer-motion';
+import { FaChevronUp } from 'react-icons/fa';
 
 const Certificates = () => {
-  useEffect(()=> {
-    Aos.init({duration: 400, once: true});
-  }, [])
   return (
     <>
-      <div className="awards-container" data-aos="fade-up">
+      <a href="#">
+            <div className="chevronup-container">
+              <FaChevronUp className="chevronup-icon"/>
+            </div>
+      </a>
+      <motion.div 
+        initial={{ x: '100vw' }}
+        animate={{ x: 0 }}
+        transition={{ type: 'spring', stiffness: 100, delay: 0.2 }}
+
+        className="awards-container">
         <h4>CERTIFICATES</h4>
         <h2>MY CERTIFICATES</h2>
         <div className="certs">
           <div>
-            <img src={webDesign} alt="web design course1" />
-                   
+            <a href="https://www.freecodecamp.org/certification/antwiebenezer/responsive-web-design"><img src={webDesign} alt="web design course1" /></a>  
           </div>
           <div>
-            <img src={javaScript} alt="javaScript course" />
+            <a href="https://www.coursera.org/account/accomplishments/certificate/NCG2M8UKH65Z"><img src={javaScript} alt="javaScript course" /></a>
           </div>
           <div>
-            <img src={webDesign2} alt="web design course2" />
+            <a href="https://www.sololearn.com/Certificate/CT-KHVQLLZH/png"><img src={webDesign2} alt="web design course2" /></a>
           </div>
           <div>
-            <img src={htmlCourse} alt="html course" />
+            <a href="https://www.sololearn.com/Certificate/CT-Y4HSQYNM/png"><img src={htmlCourse} alt="html course" /></a>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   )
 }

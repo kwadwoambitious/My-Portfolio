8f9e8fac85273c45e5c38158af82263b185d2890
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-05 17:59:13 +0000

    Made changes to this file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import About from './components/About';
 import Certificates from './components/Certificates';
 import Portfolio from './components/Portfolio';
+import { AnimatePresence } from 'framer-motion';
+import './App.css';
 
 function App() {
+  const location = useLocation();
   return (
-    <Router>
       <div className="App">
         <Header/>
-         <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route exact path='/about' element={<About/>}/>
-            <Route exact path='/portfolio' element={<Portfolio/>}/>
-            <Route exact path='/certificates' element={<Certificates/>}/>
-            <Route exact path='/contact' element={<Contact/>}/>
-         </Routes>
+         <AnimatePresence mode='wait'>
+            <Routes location={location} key={location.pathname}>
+                <Route exact path='/' element={<Home/>}/>
+                <Route exact path='/about' element={<About/>}/>
+                <Route exact path='/portfolio' element={<Portfolio/>}/>
+                <Route exact path='/certificates' element={<Certificates/>}/>
+                <Route exact path='/contact' element={<Contact/>}/>
+            </Routes>
+         </AnimatePresence>
       </div>
-    </Router>
   );
 }
 

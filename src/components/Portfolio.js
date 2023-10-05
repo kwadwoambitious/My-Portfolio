2442cf7db658203a6699fffcbff5167281627695
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-05 18:01:21 +0000

    Made changes to this file

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,24 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { motion } from 'framer-motion';
+import React from 'react';
+import { FaChevronUp } from 'react-icons/fa';
 
 const Portfolio = () => {
-  useEffect(()=> {
-    Aos.init({duration: 400});
-  }, [])
   return (
     <>
-      <div className="portfolio-container" data-aos="fade-up">
+      <a href="#">
+            <div className="chevronup-container">
+              <FaChevronUp className="chevronup-icon"/>
+            </div>
+      </a>
+      <motion.div
+        initial={{ x: '100vw' }}
+        animate={{ x: 0 }}
+        transition={{ type: 'spring', stiffness: 100, delay: 0.2 }}
+
+        className="portfolio-container">
         <h4>PORTFOLIO</h4>
         <h2>MY PROJECTS</h2>
-      </div>
+      </motion.div>
     </>
   )
 }

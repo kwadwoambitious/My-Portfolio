5b62f407f48ffc25cbe500d2e779b8bef9e45d88
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-06 18:59:03 +0000

    Made changes to this file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -54,7 +54,8 @@ const Contact = () => {
       <motion.div
         initial={{ x: '100vw' }}
         animate={{ x: 0 }}
-        transition={{ type: 'spring', stiffness: 100, delay: 0.2 }}
+        transition={{ type: 'spring', stiffness: 100, duration: 0.1, ease: [0.22, 1, 0.36, 1] }}
+        // exit= {{ x: '-100vw' }}
 
         className="contact-container">
         <h4>CONTACT</h4>

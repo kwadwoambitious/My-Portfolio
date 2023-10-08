40ab5d19ef7671062ccb0c7038e15e9ffa789f7d
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-08 13:35:23 +0000

    Made changes to this file

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
             
             className="home">
           <h1>Ebenezer Antwi</h1>
-          <h2>I'm a <span>Front-end Developer.</span></h2>
+          <h2>I'm a <span>Front-end Developerhhh</span></h2>
             <div className='social-icons'>
                     <a href="https://twitter.com/_antwiebenezer" target='_blank' rel='noreferrer'><FaTwitter className='icon'/></a>
                     <a href="https://www.linkedin.com/in/antwi-ebenezer" target='_blank' rel='noreferrer'><FaLinkedin className='icon'/></a>

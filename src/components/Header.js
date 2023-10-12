04b7330b2aac934dad179d71facb33d0f2dec96f
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-12 12:35:26 +0000

    Made changes to this file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
-import { FaAngellist } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <>
       <header>
         <div className='logo'>
-          <FaAngellist className="icon" /><Link to="/"><h1><span>EB.</span>CODES</h1></Link>
+          <Link to="/"><h1><span>EB.</span>CODES</h1></Link>
         </div>
         <NavBar/>
       </header>

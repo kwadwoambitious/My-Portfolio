937e0614dcdc66aa2a0f87424028e8e48247a33b
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-12 11:44:50 +0000

    Made changes to this file

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -16,10 +16,10 @@ const Certificates = () => {
             <div class="timeline-items">
               <div class="timeline-item">
                 <div class="timeline-dot"></div>
-                <div class="timeline-date">2015</div>
+                <div class="timeline-date">June 2023</div>
                 <div class="timeline-content">
                   <h3>PROGRAMMING WITH JAVASCRIPT</h3>
-                  <p>I was fortunate enough to partake in this course powered by Coursera. Having completed online courses on HTML and CSS, I needed to learn JavaScript to make my projects more interactive. During this course, I was exposed to vital lessons such as arrays, functions, DOM, classes, testing, etc. With the knowledge acquired from this course, I could build projects more effectively.</p>
+                  <p>I was fortunate enough to partake in this course powered by <span>Coursera.</span> Having completed online courses on HTML and CSS, I needed to learn JavaScript to make my projects more interactive. During this course, I was exposed to vital lessons such as arrays, functions, DOM, classes, testing, etc. With the knowledge acquired from this course, I could build projects more effectively.</p>
                   <a href='https://www.coursera.org/account/accomplishments/certificate/NCG2M8UKH65Z' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>
@@ -28,7 +28,7 @@ const Certificates = () => {
                 <div class="timeline-date">2016</div>
                 <div class="timeline-content">
                   <h3>RESPONSIVE WEB DESIGN</h3>
-                  <p>Building projects that were not responsive was giving me a headache. So, I decided to find an online course that would help me solve these issues. I got to know about this course offered by freeCodeCamp from a friend. I learned the best way to make my projects very responsive on various screen sizes. During this course, I was given five different projects to work on, and they were completed successfully.</p>
+                  <p>Building projects that were not responsive was giving me a headache. So, I decided to find an online course that would help me solve these issues. I got to know about this course offered by <span>freeCodeCamp</span> from a friend. I learned the best way to make my projects very responsive on various screen sizes. During this course, I was given five different projects to work on, and they were completed successfully.</p>
                   <a href='https://www.freecodecamp.org/certification/antwiebenezer/responsive-web-design' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>

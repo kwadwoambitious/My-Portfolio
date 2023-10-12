4cf903fbcdf026186cdb4f573ff8d3cdddab1518
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-10-12 11:35:15 +0000

    Made changes to this file

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -20,7 +20,7 @@ const Certificates = () => {
                 <div class="timeline-content">
                   <h3>PROGRAMMING WITH JAVASCRIPT</h3>
                   <p>I was fortunate enough to partake in this course powered by Coursera. Having completed online courses on HTML and CSS, I needed to learn JavaScript to make my projects more interactive. During this course, I was exposed to vital lessons such as arrays, functions, DOM, classes, testing, etc. With the knowledge acquired from this course, I could build projects more effectively.</p>
-                  <a href='https://www.coursera.org/account/accomplishments/certificate/NCG2M8UKH65Z' rel='noreferrer'>CHECK IT</a>
+                  <a href='https://www.coursera.org/account/accomplishments/certificate/NCG2M8UKH65Z' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>
               <div class="timeline-item">
@@ -29,7 +29,7 @@ const Certificates = () => {
                 <div class="timeline-content">
                   <h3>RESPONSIVE WEB DESIGN</h3>
                   <p>Building projects that were not responsive was giving me a headache. So, I decided to find an online course that would help me solve these issues. I got to know about this course offered by freeCodeCamp from a friend. I learned the best way to make my projects very responsive on various screen sizes. During this course, I was given five different projects to work on, and they were completed successfully.</p>
-                  <a href='https://www.freecodecamp.org/certification/antwiebenezer/responsive-web-design' rel='noreferrer'>CHECK IT</a>
+                  <a href='https://www.freecodecamp.org/certification/antwiebenezer/responsive-web-design' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>
               <div class="timeline-item">
@@ -38,7 +38,7 @@ const Certificates = () => {
                 <div class="timeline-content">
                   <h3>RESPONSIVE WEB DESIGN</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                  <a href='https://www.sololearn.com/Certificate/CT-KHVQLLZH/png' rel='noreferrer'>CHECK IT</a>
+                  <a href='https://www.sololearn.com/Certificate/CT-KHVQLLZH/png' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>
               <div class="timeline-item">
@@ -47,7 +47,7 @@ const Certificates = () => {
                 <div class="timeline-content">
                   <h3>HTML</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                  <a href='https://www.sololearn.com/Certificate/CT-Y4HSQYNM/png' rel='noreferrer'>CHECK IT</a>
+                  <a href='https://www.sololearn.com/Certificate/CT-Y4HSQYNM/png' target='_blank' rel='noreferrer'>CHECK IT</a>
                 </div>
               </div>
             </div>
